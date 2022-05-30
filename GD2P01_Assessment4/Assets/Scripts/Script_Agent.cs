757e19a956d31daa28cedc75211dda4f86347dfd
trandunz <54924605+trandunz@users.noreply.github.com>
2022-05-31 10:29:21 +1200

    + Base Project 2D

---
+++ GD2P01_Assessment4/Assets/Scripts/Script_Agent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_Agent : MonoBehaviour
+{
+    Vector3 Velocity;
+    Vector3 Acceleration;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ApplyForce(Vector2 _force)
+    {
+
+    }
+}
---
+++ GD2P01_Assessment4/Assets/Scripts/Script_MatchWidth.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_MatchWidth : MonoBehaviour
+{
+    // Set this to the in-world distance between the left & right edges of your scene.
+    [SerializeField] float SceneWidth = 10;
+
+    Camera camera;
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+    }
+
+    // Adjust the camera's height so the desired scene width fits in view
+    // even if the screen/window size changes dynamically.
+    void Update()
+    {
+        float unitsPerPixel = SceneWidth / Screen.width;
+
+        float desiredHalfHeight = 0.5f * unitsPerPixel * Screen.height;
+
+        camera.orthographicSize = desiredHalfHeight;
+    }
+}
